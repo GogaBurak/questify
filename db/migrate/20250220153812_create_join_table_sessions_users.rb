eb7c3dda23ad2ddad3a784af833a8e4fa25dbd54
GogaBurak <maestro.prokrastinator@gmail.com>
2025-02-26 23:07:20 +0300

    fix rubocop

--- db/migrate/20250220153812_create_join_table_sessions_users.rb
+++ db/migrate/20250220153812_create_join_table_sessions_users.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableSessionsUsers < ActiveRecord::Migration[8.0]
   def change
     create_join_table :sessions, :players do |t|
-      t.index [:session_id, :player_id]
-      t.index [:player_id, :session_id]
+      t.index [ :session_id, :player_id ]
+      t.index [ :player_id, :session_id ]
     end
   end
 end
